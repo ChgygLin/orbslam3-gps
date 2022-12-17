1059140fcefe7b08744827aeaa4b11db6097a49b
stacowt.lin@gmail.com <stacowt.lin@gmail.com>
2022-12-17 16:53:41 +0800

    优化误差可视化显示

--- show.py
+++ show.py
@@ -1,33 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+import os
 
 import pandas as pd
 
-err=pd.read_csv("./TW_err.txt", names=['id','res', 'lat', 'lon', 'alt'], delimiter=' ')
+if os.path.exists('./png') is not True:
+    os.mkdir('./png')
 
+err_file=sys.argv[1]
+file_name=err_file.split('/')[-1].split('.')[0]
+
+err=pd.read_csv(err_file, names=['id','res', 'lat', 'lon', 'alt'], delimiter=' ')
+
+iend=300
 
 #fig = plt.figure()
 x=err['id'].values-err['id'][0]
-res=err['res'].values
-lat=err['lat'].values
-lon=err['lon'].values
-alt=err['alt'].values
+res=err['res'][:iend].values
+lat=err['lat'][:iend].values
+lon=err['lon'][:iend].values
+alt=err['alt'][:iend].values
 
 
-avg_res=err['res'].mean()
-avg_lat=err['lat'].mean()
-avg_lon=err['lon'].mean()
-avg_alt=err['alt'].mean()
+avg_res=res.mean()
+avg_lat=lat.mean()
+avg_lon=lon.mean()
+avg_alt=alt.mean()
 
-
-plt.plot(res[:1000], label='error: '+ "{:.2f}".format(avg_res) + 'm' )
-plt.plot(lat[:1000], color='r', label='lat error: '+ "{:.2f}".format(avg_lat) + 'm')
-plt.plot(lon[:1000], color='g', label='lon error: '+ "{:.2f}".format(avg_lon) + 'm')
-plt.plot(alt[:1000], color='b', label='alt error: '+ "{:.2f}".format(avg_alt) + 'm')
+plt.plot(res[:iend], label='error: '+ "{:.2f}".format(avg_res) + 'm' )
+plt.plot(lat[:iend], color='r', label='lat error: '+ "{:.2f}".format(avg_lat) + 'm')
+plt.plot(lon[:iend], color='g', label='lon error: '+ "{:.2f}".format(avg_lon) + 'm')
+plt.plot(alt[:iend], color='b', label='alt error: '+ "{:.2f}".format(avg_alt) + 'm')
 
 
 plt.legend()
-plt.title('camera postion error')
+plt.title('camera postion error: ' + file_name)
 plt.xlabel('image')
 plt.ylabel('error(m)')
+
+plt.savefig('png/'+file_name+'.png')
 plt.show()
